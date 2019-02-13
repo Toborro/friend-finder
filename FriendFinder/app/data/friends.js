8055586e11a2f387081eb4a481fbefbd850817ee
Toborro <zephyrthewest@gmail.com>
2019-02-12 20:04:30 -0500

    first commit

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Luke Skywalker',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Luke_Skywalker.png/220px-Luke_Skywalker.png',
+		scores: [
+			'5',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Darth Vader',
+		photo: 'https://boygeniusreport.files.wordpress.com/2015/08/darth-vader.jpg?quality=98&strip=all&w=782',
+		scores: [
+			'5',
+			'1',
+			'5',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Yoda',
+		photo: 'https://lumiere-a.akamaihd.net/v1/images/Yoda-Retina_2a7ecc26.jpeg?region=0%2C0%2C1536%2C864&width=960',
+		scores: [
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Ben Kenobi',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/3/32/Ben_Kenobi.png',
+		scores: [
+			'5',
+			'5',
+			'1',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Emperor Palpatine',
+		photo: 'https://starwarsblog.starwars.com/wp-content/uploads/2014/12/Palpatine-1536x864-261271428363.jpg',
+		scores: [
+			'5',
+			'1',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Han Solo',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/be/Han_Solo_depicted_in_promotional_image_for_Star_Wars_%281977%29.jpg/220px-Han_Solo_depicted_in_promotional_image_for_Star_Wars_%281977%29.jpg',
+		scores: [
+			'1',
+			'3',
+			'5',
+			'5',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Princess Leia Organa',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/1/1b/Princess_Leia%27s_characteristic_hairstyle.jpg/220px-Princess_Leia%27s_characteristic_hairstyle.jpg',
+		scores: [
+			'3',
+			'3',
+			'4',
+			'3',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Lando Calrissian',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/c/cb/Lando6-2.jpg',
+		scores: [
+			'2',
+			'3',
+			'3',
+			'4',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Boba Fett',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Star_Wars_and_the_Power_of_Costume_July_2018_28_%28Boba_Fett%27s_costume%2C_helmet%2C_and_jetpack_from_Episode_VI%29.jpg/220px-Star_Wars_and_the_Power_of_Costume_July_2018_28_%28Boba_Fett%27s_costume%2C_helmet%2C_and_jetpack_from_Episode_VI%29.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Dak (Mr. I feel so good I can take the whole empire on by myself)',
+		photo: 'https://lumiere-a.akamaihd.net/v1/images/e5d_ia_1559_acf49910.jpeg?region=285%2C0%2C1449%2C815&width=960',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
